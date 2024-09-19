368f8eeae21f13cfd6c2548a2bd6de8ec09ffe8c
Mest <mestevenyoutubing@gmail.com>
2024-09-19 23:28:38 +0100

    Fixed perk display (#1)
    * Fixed perk display
    
    * Updated to use properties instead of Method

--- ViewPlayerPerks/Menu.cs
+++ ViewPlayerPerks/Menu.cs
@@ -1,8 +1,11 @@
-﻿using ExitGames.Client.Photon;
+using ExitGames.Client.Photon;
 using Gameplay.Perks;
 using Photon.Realtime;
 using ResourceAssets;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using UI.Token;
 using UnityEngine;
 using VoidManager.CustomGUI;
 
@@ -13,6 +16,24 @@ namespace ViewPlayerPerks
         private GUISkin _cachedSkin;
         private GUISkin _defaultSkin;
         private int spentPerks;
+
+        private int PerkPoints;
+        private int PlayerLevel;
+        private List<PerkRef> activePerks;
+        Dictionary<GUIDUnion, int> buffLevels;
+        public override void Refresh(Player selectedPlayer)
+        {
+            if (selectedPlayer == null) return;
+            object obj;
+            if (selectedPlayer.CustomProperties.TryGetValue("RP_PR", out obj)) PerkPoints = (int)obj + 1;
+            if (selectedPlayer.CustomProperties.TryGetValue("RP_FR", out obj)) PlayerLevel = (int)obj;
+            activePerks = GetActivePerks(selectedPlayer.CustomProperties);
+            PerkRef Engineer = activePerks[0]; PerkRef Scavenger = activePerks[3];
+            activePerks.RemoveAt(3); activePerks.RemoveAt(0);
+            activePerks.Insert(2, Engineer); activePerks.Insert(3, Scavenger);
+            buffLevels = GetPerkBuffLevels(selectedPlayer.CustomProperties);
+        }
+        
         public override void Draw(Player selectedPlayer)
         {
             _cachedSkin = GUISkin.Instantiate(GUI.skin);
@@ -22,14 +43,11 @@ namespace ViewPlayerPerks
                 {
                     using (new GUILayout.HorizontalScope())
                     {
-                        object obj;
-                        if (selectedPlayer.CustomProperties.TryGetValue("RP_PR", out obj)) GUILayout.Label($"Perk Points: {((int)obj + 1) - spentPerks}");
-                        if (selectedPlayer.CustomProperties.TryGetValue("RP_FR", out obj)) GUILayout.Label($"Player Level: {(int)obj}");
+                        GUILayout.Label($"Perk Points: {PerkPoints - spentPerks}");
+                        GUILayout.Label($"Player Level: {PlayerLevel}");
+                        if (GUILayout.Button("Refresh")) Refresh(selectedPlayer);
                     }
-                    List<PerkRef> activePerks = [.. GetActivePerks(selectedPlayer.CustomProperties)];
-                    PerkRef Engineer = activePerks[0]; PerkRef Scavenger = activePerks[3];
-                    activePerks.RemoveAt(3); activePerks.RemoveAt(0);
-                    activePerks.Insert(2, Engineer); activePerks.Insert(3, Scavenger);
+
                     using (new GUILayout.HorizontalScope())
                     {
                         spentPerks = 0;
@@ -44,10 +62,14 @@ namespace ViewPlayerPerks
                                 foreach (PerkBuffRef perkBuffRef in perkref.Asset.Buffs)
                                 {
                                     PerkBuff asset = perkBuffRef.Asset;
-                                    if (asset != null)
+                                    if (asset != null && asset.DisplayName != "Coming Soon!")
                                     {
-                                        int buffLevel = asset.GetBuffLevel();
-                                        if (asset.DisplayName != "Coming Soon!" && buffLevel > 0)
+                                        int buffLevel = 0;
+                                        if (buffLevels.ContainsKey(perkBuffRef.AssetGuid))
+                                        {
+                                            buffLevel = buffLevels[perkBuffRef.AssetGuid];
+                                        }
+                                        if (buffLevel > 0)
                                         {
                                             using (new GUILayout.HorizontalScope())
                                             {
@@ -64,20 +86,49 @@ namespace ViewPlayerPerks
                 }
             }
         }
+        
         public List<PerkRef> GetActivePerks(Hashtable hashtable)
         {
             List<PerkRef> perks = new List<PerkRef>();
 
             object obj;
-            if (!hashtable.TryGetValue("OCP", out obj) && !hashtable.TryGetValue("ACP", out obj))
+            if (!hashtable.TryGetValue("ACP", out obj))
                 return perks;
 
             int[] array = (int[])obj;
-            for (int i = 0; i < array.Length; i += 4)
+            for (int i = 0; i < array.Length / 4; i++)
             {
-                perks.Add(new PerkRef(new int[] { array[i], array[i + 1], array[i + 2], array[i + 3] }));
+                perks.Add(new PerkRef(new int[]
+                {
+                    array[i * 4],
+                    array[i * 4 + 1],
+                    array[i * 4 + 2],
+                    array[i * 4 + 3]
+                }));
             }
             return perks;
+        }
+
+        public Dictionary<GUIDUnion, int> GetPerkBuffLevels(Hashtable hashtable)
+        {
+            Dictionary<GUIDUnion, int> perkLevels = new Dictionary<GUIDUnion, int>();
+
+            object obj;
+            if (!hashtable.TryGetValue("PBO", out obj) && !hashtable.TryGetValue("PBL", out obj))
+                return perkLevels;
+
+            int[] array = (int[])obj;
+            for (int i = 0; i < array.Length / 5; i++)
+            {
+                perkLevels.Add(new GUIDUnion(new int[]
+                {
+                    array[i * 5],
+                    array[i * 5 + 1],
+                    array[i * 5 + 2],
+                    array[i * 5 + 3]
+                }), array[i * 5 + 4]);
+            }
+            return perkLevels;
         }
     }
 }
